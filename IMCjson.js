fe619b900e2e9c1b2dc427e680b052d35a6e9172
Misaelchavez16 <75141227+Misaelchavez16@users.noreply.github.com>
2022-03-10 21:53:29 -0600

    Imagenes a IMC

--- IMCjson.js
+++ IMCjson.js
@@ -2,6 +2,7 @@ function procesarFormulario(){
     var peso = $("#peso").val()
     var estatura = $("#estatura").val()
     
+    
 
     /*var peso = document.getElementById("peso").value
     var estatura = document.getElementById("estatura").value*/
@@ -15,21 +16,27 @@ function procesarFormulario(){
 
     if (imc < 18.6){
         mensaje = "Tu IMC es " + imc + " se concidera bajo de peso :)";
+        $("#imagen").css("content", "url('Imagenes/BajoPeso.png')")
     }
     else if (imc <25){
         mensaje = "Tu IMC es " + imc + " se considera peso saludable MUY BIEN :D";
+        $("#imagen").css("content", "url('Imagenes/Gigachad.jpg')")
     }
     else if (imc <30){
         mensaje = "Tu IMC es " + imc + " se considera sobre peso :)";
+        $("#imagen").css("content", "url('Imagenes/SobrePeso.png')")
     }
     else if (imc <35){
         mensaje = "Tu IMC es " + imc + " se considera obesidad de grado 1 :|";
+        $("#imagen").css("content", "url('Imagenes/ObesidadGrado.png')")
     }
     else if (imc <40){
         mensaje = "Tu IMC es " + imc + " se considera obesidad de grado 2 :(";
+        $("#imagen").css("content", "url('Imagenes/ObesidadGrado.png')")
     }
     else if (imc >=40){
         mensaje = "Tu IMC es " + imc + " se considera obesidad morbida D:";
+        $("#imagen").css("content", "url('Imagenes/Morbida.png')")
     }
 
     console.log(mensaje)
